73002ef31152434fa6aada21cec5e3a4e1215121
edrei giron <egironl12@miumg.edu.gt>
2024-03-12 16:38:32 -0600

    Creacion Clase Iniciar Sesion y Layout Iniciar sesion

---
+++ UMessaGe/app/src/main/java/com/umg/umessage/IniciarSesionActivity.kt
@@ -0,0 +1,12 @@
+package com.umg.umessage
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class IniciarSesionActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
