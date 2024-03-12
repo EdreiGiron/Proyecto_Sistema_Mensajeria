e82735bdedf0fa89375daa2aad722be5f13b8240
edrei giron <egironl12@miumg.edu.gt>
2024-03-11 21:42:54 -0600

    Creación del activity login

---
+++ UMessaGe/app/src/main/java/com/umg/umessage/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.umg.umessage.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
